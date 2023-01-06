66fe6f4e05d1e8bdf6ee7f4973432fa868ece453
romansmolin <98619713+romansmolin@users.noreply.github.com>
2023-01-06 13:43:26 +0200

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+/*MENU SHOW FUNCTION**/
+
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', () =>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+
+showMenu('nav-toggle', 'nav-menu')
+
+/*Active and remove menu */
+const navLink = document.querySelectorAll('.nav__link')
+function linkAction(){
+    navLink.forEach(n => n.classList.remove('active'))
+    this.classList.add('active')
+
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show');
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/* EMAIL*/
+function sendEmail(){
+    alert("Your message has been sent succesfully")
+}
